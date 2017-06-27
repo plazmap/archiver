7010272e5c720e386a2eb406d78babd6a03352e3
Plazma <clem-roux@hotmail.fr>
2017-06-27 16:47:26 +0200

    Add register

--- views/register.php
+++ views/register.php
@@ -1,9 +1,22 @@
 <?php if (isset($err)) { ?>
 <p><?= $err ?></p>
 <?php } ?>
+
+<?php if (isset($err2)) { ?>
+<p><?= $err2 ?></p>
+<?php } ?>
+
+
+
+ <?php if (isset($errors)) {
+   ?><p><?php echo $errors?></p>
+<?php } ?>
+
+
+
 <form method="POST">
-	<input type="text" name="login" placeholder="login" required>
-	<input type="password" name="password" placeholder="password" required>
-    <input type="password" name="pass_verif" placeholder="verify password" required>
+    <input type="text" name="login" placeholder="login" required>
+    <input type="password" name="password" placeholder="password" required>
+    <input type="password" name="password_confirmation" placeholder="verify password" required>
     <button type="submit">register</button>
 </form>
